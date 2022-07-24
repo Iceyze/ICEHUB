99b9a61cc5aed0ac9c0b28c9c2a7252d5f570deb
Iceyze <60437633+Iceyze@users.noreply.github.com>
2022-07-23 22:53:25 -0700

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,16 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/kav"))()
+local Window = Library.CreateLib("ICEHUB", "Serpent")
+local Tab = Window:NewTab("OPTIONS")
+local Section = Tab:NewSection("MAIN")
+
+local function AIMLABS1()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Iceyze/aimlabs/main/aimlabs_v2.5(1).lua"))()
+end
+
+Section:NewButton("INFINITE YIELD", "ButtonInfo", function()
+     loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
+end)
+
+Section:NewButton("AIMLABS", "ButtonInfo", function()
+    AIMLABS1()
+end)
